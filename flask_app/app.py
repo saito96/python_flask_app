80a4117645cb5fac11962709246b178de205ef29
saito <vqcuong96@gmail.com>
2018-01-24 17:49:42 +0700

    test

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import os
 
+# template_dir = os.path.dirname(os.path.abspath(__file__))
+# template_dir = os.path.join(template_dir, 'frontend')
+# template_dir = os.path.join(template_dir, 'templates')
 app = Flask(__name__)
+
+
 @app.route("/")
 def main():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
